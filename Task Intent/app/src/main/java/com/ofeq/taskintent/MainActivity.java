b8724b501aaa8bb0493a1250a873010887111797
achmadtaufiqhidayat <ahmadtaufiqhidayat19@gmail.com>
2022-01-04 22:29:08 +0700

    mencoba intent ke whatsapp

--- Task Intent/app/src/main/java/com/ofeq/taskintent/MainActivity.java
+++ Task Intent/app/src/main/java/com/ofeq/taskintent/MainActivity.java
@@ -3,16 +3,21 @@ package com.ofeq.taskintent;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.net.Uri;
 import android.os.Bundle;
+import android.provider.Settings;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import java.io.UnsupportedEncodingException;
+import java.net.URLEncoder;
+
 public class MainActivity extends AppCompatActivity {
 //deklarasi
-    Button myButton, btnSubmit;
+    Button myButton, btnSubmit, btnWa;
     EditText edtText, name;
     private String KEY_NAME = "NAMA";
 
@@ -28,6 +33,7 @@ public class MainActivity extends AppCompatActivity {
         activity_main.xml*/
         myButton = findViewById(R.id.myButton);
         edtText = findViewById(R.id.edtText);
+        btnWa = findViewById(R.id.btnwa);
         /*memberikan action saat diklik
         * dalam kasus ini, kita mengisi url
         * kemudian menekan button
@@ -71,12 +77,35 @@ public class MainActivity extends AppCompatActivity {
             e.printStackTrace();
             Toast.makeText(getApplication(), "ERROR, TRY AGAIN !",Toast.LENGTH_SHORT);
         }
-    }
 
+
+       }
 
 
 
         });
-        
+        //disini code untuk direct wa
+
+
+
+        btnWa.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                //String link = "https://api.whatsapp.com/send?phone=+6281249345670";
+                String link= null;
+                try {
+                    link = "https://api.whatsapp.com/send?phone=+6281249345670"+"&text=Hallo.\n" +
+                            "Saya ingin bertanya ketersediaan item di POLOSKUY!";
+                    Intent intent = new Intent(Intent.ACTION_VIEW, Uri.parse(link));
+                    startActivity(intent);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    Toast.makeText(getApplication(), "Gagal membuka WhatsApp", Toast.LENGTH_SHORT).show();
+                }
+
+            }
+        });
+
+
     }
 }
